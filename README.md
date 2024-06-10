76bed5960ee7d64f9647ab7daf6ea8d83fd61a42
Kelvin Lee <kiyolee@gmail.com>
2024-06-10 21:52:15 +1000

    Update to 3.1.6

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 openssl-3 Windows build with Visual Studio.
 
-This version is openssl-3.1.5.
+This version is openssl-3.1.6.
 
 To build, simply open the required solution file, and
 you know how to use Visual Studio, right?
--- ms/arm64/static/configdata.pm
+++ ms/arm64/static/configdata.pm
@@ -163,7 +163,7 @@ our %config = (
     ],
     "dynamic_engines" => "0",
     "ex_libs" => [],
-    "full_version" => "3.1.5",
+    "full_version" => "3.1.6",
     "includes" => [],
     "lflags" => [],
     "lib_defines" => [
@@ -214,10 +214,10 @@ our %config = (
     "openssl_sys_defines" => [],
     "openssldir" => "",
     "options" => "--prefix=C:\\Program Files\\OpenSSL-3 --with-zlib-include=..\\zlib --with-zlib-lib=..\\zlib\\build\\ARM64\\Release\\libz-static.lib enable-zlib no-acvp-tests no-afalgeng no-asan no-asm no-buildtest-c++ no-crypto-mdebug no-crypto-mdebug-backtrace no-devcryptoeng no-dynamic-engine no-ec_nistp_64_gcc_128 no-egd no-external-tests no-fips no-fips-securitychecks no-fuzz-afl no-fuzz-libfuzzer no-ktls no-loadereng no-md2 no-msan no-rc5 no-sctp no-shared no-ssl3 no-ssl3-method no-trace no-ubsan no-unit-test no-uplink no-weak-ssl-ciphers no-zlib-dynamic",
-    "patch" => "5",
+    "patch" => "6",
     "perl_archname" => "MSWin32-x64-multi-thread",
     "perl_cmd" => "C:\\Strawberry\\perl\\bin\\perl.exe",
-    "perl_version" => "5.38.0",
+    "perl_version" => "5.38.2",
     "perlargv" => [
         "--prefix=C:\\Program Files\\OpenSSL-3",
         "--with-zlib-include=..\\zlib",
@@ -267,11 +267,11 @@ our %config = (
     "prerelease" => "",
     "processor" => "",
     "rc4_int" => "unsigned char",
-    "release_date" => "30 Jan 2024",
+    "release_date" => "4 Jun 2024",
     "shlib_version" => "3",
     "sourcedir" => ".",
     "target" => "VC-WIN64-ARM",
-    "version" => "3.1.5"
+    "version" => "3.1.6"
 );
 our %target = (
     "AR" => "lib",
@@ -370,6 +370,7 @@ our @disablables = (
     "asan",
     "asm",
     "async",
+    "atexit",
     "autoalginit",
     "autoerrinit",
     "autoload-config",
